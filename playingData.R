a9ccb5ca10eb1815da8577a9d843dbc3a5860426
sinH15 <sinh15@users.noreply.github.com>
2015-12-10 00:15:00 +0100

    first tryout in R

--- playingData.R
+++ playingData.R
@@ -7,3 +7,8 @@ library(dplyr)
 dd <- read.xlsx("data.xlsx", 1)
 dd[, "name"] <- as.character(dd[, "name"])
 names(dd)
+
+## CHECK MEAN, MIN, MAX minearls
+mean(dd[dd$transformation == 0 & dd$type == "unit", "minerals"])
+min(dd[dd$transformation == 0 & dd$type == "unit", "minerals"])
+max(dd[dd$transformation == 0 & dd$type == "unit", "minerals"])
