91c9f65e5d82bf102745ba122f3c9cb6e96375e6
sinh15 <sinh15@users.noreply.github.com>
2015-12-10 17:06:30 +0100

    New Lines

--- playingData.R
+++ playingData.R
@@ -1,4 +1,5 @@
 ## Loading Required Libraries
+#Sys.setenv(JAVA_HOME='C:\\Program Files\\Java\\jre1.8.0_66')
 library(xlsx)
 library(dplyr)
 
@@ -12,3 +13,12 @@ names(dd)
 mean(dd[dd$transformation == 0 & dd$type == "unit", "minerals"])
 min(dd[dd$transformation == 0 & dd$type == "unit", "minerals"])
 max(dd[dd$transformation == 0 & dd$type == "unit", "minerals"])
+
+## Play with NA columns and intersects
+x <- dd[!is.na(dd$airAttack), "name"]
+y <- dd[!is.na(dd$groundAttack), "name"]
+z <- Reduce(intersect, list(x, y))
+
+groundDPS <- dd$groundAttack*dd$gaMultiplier/dd$groundCD
+airDPS <- dd$airAttack*dd$aaMultiplier/dd$airCD
+res <- rbind(dd$name, groundDPS, airDPS)
