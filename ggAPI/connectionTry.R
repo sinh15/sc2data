35fe82b348361ffad641eb9ccbade2fcd76d975a
Sergi Porras <contactsinh15@gmail.com>
2015-12-12 17:14:43 +0100

    new lines

--- ggAPI/connectionTry.R
+++ ggAPI/connectionTry.R
@@ -14,8 +14,27 @@ gameJSON <- fromJSON(gameJSON)
 names(gameJSON)
 View(lapply(gameJSON, class))
 
-## From simple details get playerIDs, names, race & winner
-### "entities" column 16. DATA FRAME
-entities <- gameJSON[["entities"]]
-identity <- entities[, "identity"]
+## SIMPLE DETAILS: Get relevant information
+gameID <- gameJSON[["id"]]
+gameDate <- as.Date(gameJSON[["ended_at"]])
+gameCategory <- gameJSON[["category"]]
+gameType <- gameJSON[["game_type"]]
+gameDuration <- gameJSON[["duration_seconds"]]
+gameMapID <- gameJSON[["map"]][["id"]]
+gameMapName <- gameJSON[["map"]][["name"]]
+replayID <- gameJSON[["replays"]][, 1]
+replayHash <- gameJSON[["replays"]][, 2]
+replayURL <- gameJSON[["replays"]][, 3]
 
+### entities fields
+entities <- gameJSON[["entities"]]
+id_1 <- entities[, "identity"][, "id"][1]
+id_2 <- entities[, "identity"][, "id"][2]
+bnet_id_1 <- entities[, "identity"][, "bnet_id"][1]
+bnet_id_2 <- entities[, "identity"][, "bnet_id"][2]
+p1_name <- entities[, "identity"][, "name"][1]
+p2_name <- entities[, "identity"][, "name"][2]
+p1_race <- entities[, "race"][1]
+p2_race <- entities[, "race"][2]
+p1_win <- entities[, "win"][1]
+p2_win <- entities[, "win"][2]
